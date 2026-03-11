ec46b710b3bce068c54601ffa8b080910c639357
DevHanza <hansana123dasanayake@gmail.com>
2026-03-11 14:27:37 +0530

    DSA: Two Sum 2 (js)

---
+++ Data Structures & Algorithms/Algorithms/8. Two Sum/TwoSum-2.js
@@ -0,0 +1,23 @@
+// :::::::::::::::::::::::::::::::::
+// :: Method 2 : With Hash Tables
+// :::::::::::::::::::::::::::::::::
+
+function twoSum(nums, target) {
+  const numMap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (complement in numMap && numMap[complement] !== i) {
+      return [numMap[complement], i];
+    }
+
+    numMap[nums[i]] = i;
+  }
+
+  return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 3], 6));
