422ec71413916029ddda8212209dd463d13e1e6c
DevHanza <hansana123dasanayake@gmail.com>
2026-04-04 17:48:51 +0530

    DSA: Selection Sort

---
+++ Data Structures & Algorithms/Algorithms/14. Selection Sort/selection-sort.js
@@ -0,0 +1,22 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    //
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      //
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+      //
+    }
+
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    //
+  }
+  return arr;
+}
+
+console.log(selectionSort([6, 5, 4, 3, 2, 1]));
+console.log(selectionSort([5, 3, 8, 1]));
+console.log(selectionSort([7, 5, 3, 9, 2, 1]));
