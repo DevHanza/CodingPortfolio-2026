a8ab9dc1f66707f2a8a8b4964d629329913be3db
DevHanza <hansana123dasanayake@gmail.com>
2026-04-08 16:20:43 +0530

    DSA: Merge Sort

---
+++ Data Structures & Algorithms/Algorithms/16. Merge Sort/merge-sort.js
@@ -0,0 +1,38 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+//
+
+function merge(left, right) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+
+  result.push(...left.slice(i));
+  result.push(...right.slice(j));
+
+  return result;
+  // return result.concat(left.slice(i)).concat(right.slice(j));
+}
+
+console.log(mergeSort([5, 3, 8, 1, 2]));
+console.log(mergeSort([3, 1, 2, 4, 1, 5, 2, 6, 4]));
